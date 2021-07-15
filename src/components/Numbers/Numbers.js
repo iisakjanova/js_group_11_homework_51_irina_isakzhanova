74a6e8f75b128f3490edd60e637d29d7d0ef7754
Irina Isakzhanova <irina.isakjanova@gmail.com>
2021-07-15 15:07:04 +0600

    Created method getNumbers

--- src/components/Numbers/Numbers.js
+++ src/components/Numbers/Numbers.js
@@ -3,14 +3,37 @@ import React from 'react';
 class Numbers extends React.Component {
     state = {
         numbers: []
-    };
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            numbers: this.getNumbers()
+        }
+    }
+
+    getNumbers() {
+        let numbersArray = [];
+
+        while (numbersArray.length < 5) {
+            const number = Math.floor(Math.random() * 32 + 5);
+
+            if (!numbersArray.includes(number)) {
+                numbersArray.push(number);
+            }
+        }
+
+        return numbersArray.sort((a, b) => a > b ? 1 : -1);
+    }
 
     render() {
         return (
-            <div className="numbers">
-                {this.state.numbers.map((item) => (
-                    <div className="number">{item}</div>
-                ))}
+            <div>
+                <div className="numbers">
+                    {this.state.numbers.map((item) => (
+                        <div className="number" key={item}>{item}</div>
+                    ))}
+                </div>
             </div>
         );
     }
